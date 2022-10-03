492af0be288574b981f64b549a2f64c31fd2f66c
thanhloc <thanhlocdev@gmail.com>
2022-10-03 22:34:30 +0700

    update UI conduit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,13 +4,25 @@ import { AppRoutingModule } from "./app-routing.module";
 import { AppComponent } from "./app.component";
 import { HeaderComponent } from "./core/layout/header/header.component";
 import { FooterComponent } from './core/layout/footer/footer.component';
+import { NewFeedComponent } from './features/new-feed/new-feed.component';
+import { TabToggleComponent } from './shared/components/tab-toggle/tab-toggle.component';
+import { ArticleListComponent } from './shared/components/article-list/article-list.component';
+import { ArticleComponent } from './shared/components/article/article.component';
+import { SignInComponent } from './features/sign-in/sign-in.component';
+import { SignUpComponent } from './features/sign-up/sign-up.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    FooterComponent
+    FooterComponent,
+    NewFeedComponent,
+    TabToggleComponent,
+    ArticleListComponent,
+    ArticleComponent,
+    SignInComponent,
+    SignUpComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/shared/components/tab-toggle/tab-toggle.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-tab-toggle',
+  templateUrl: './tab-toggle.component.html',
+  styleUrls: ['./tab-toggle.component.scss']
+})
+export class TabToggleComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
